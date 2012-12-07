021939b2372ab95da5856e69f35c3f5d50cc0170
Marco Munizaga <marco.munizaga@grooveshark.com>
2012-12-07 01:54:10 -0500

    removed useless files

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :license {:name "GPL"
             :url "http://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.4.0"] [criterium "0.3.1"] [quil "1.6.0"]]
-  :main beadgame.chainshot3)
+  :main beadgame.chainshot )
 
